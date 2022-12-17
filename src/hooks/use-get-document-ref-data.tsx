c8d09aa17f3568e4d1658d2f44d1d12b61d154a0
stankamchev <stanimir.kamchev@gmail.com>
2022-12-17 12:22:58 +0200

    remove unused hook

--- src/hooks/use-get-document-ref-data.tsx
+++
@@ -1,7 +0,0 @@
-import { DocumentData, DocumentReference, getDocs, Query } from "firebase/firestore";
-
-const useGetQueryData = <T extends {}>(reference: DocumentReference<DocumentData>) => {
-
-};
-
-export default useGetQueryData;
