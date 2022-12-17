1dc335cd431d3d9dadcc33b32169e21bed790b08
stankamchev <stanimir.kamchev@gmail.com>
2022-12-17 13:35:14 +0200

    add try catch to get profile pic hook

--- src/hooks/use-get-user-profile-picture.tsx
+++ src/hooks/use-get-user-profile-picture.tsx
@@ -4,10 +4,15 @@ import { storage } from "../config/firebase";
 
 const useGetUserProfilePicture = async (currentUserId: string) => {
   if (currentUserId) {
-    const imageName = `profile-pictures/${currentUserId}`;
-    const imageRef = ref(storage, imageName);
-    const uploadedImageURL = await getDownloadURL(imageRef);
-    return uploadedImageURL;
+    try {
+      const imageName = `profile-pictures/${currentUserId}`;
+      const imageRef = ref(storage, imageName);
+      const uploadedImageURL = await getDownloadURL(imageRef);
+      return uploadedImageURL;
+    } catch (error) {
+      console.error((error as Error).message);
+      return "./anonymous-pic.png";
+    }
   }
 };
 
